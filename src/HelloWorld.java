1d9c2175315111413144e9921e4dced1056dc07f
MiraPratt <joseph.pratt@case.edu>
2018-12-22 17:44:36 -0500

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		print();
+	}
+	
+	public static void print() {
+		System.out.println("Goodbye World");
+	}
+	
+}
